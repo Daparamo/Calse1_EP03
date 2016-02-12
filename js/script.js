5ce59237504d8d500323d880878510e86efc7035
Daparamo <daparamo@hotmail.com>
2016-02-11 20:28:26 -0500

    EP03 - Clase1 Cloud9
    Pagina del Grupo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function()
+{
+	//alert("hola Mundo");
+	var nombre="";
+	var ape="";
+	var email="";
+	var date="";
+	swal("Bienvenido", "Hakerman!!","success");
+	$("#boton").click(function(event) {
+		nombre = $("#Nombre").
+		console.log("Nombre "+nombre);
+	});
+});
